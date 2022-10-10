ce8548cbb32953e80a40c417b88414e043e2e38d
shiwei.fu <shiwei.fu@thoughtworks.com>
2022-10-10 15:47:51 +0800

    feat:  render title when printReceipt

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,5 +1,5 @@
 import {loadAllItems, loadPromotions} from './Dependencies'
 
 export function printReceipt(tags: string[]): string {
-  return `***<store earning no money>Receipt ***`
+  return '***<store earning no money>Receipt ***'
 }
