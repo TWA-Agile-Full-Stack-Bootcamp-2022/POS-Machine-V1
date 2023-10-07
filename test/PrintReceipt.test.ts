a43b99a3b157306907089b2d2ea4a111f53ae1f1
Hong Wu <hong.wu@rea-group.com>
2023-10-07 15:09:28 +0800

    test: should print receipt without promotion when print receipt

--- test/PrintReceipt.test.ts
+++ test/PrintReceipt.test.ts
@@ -19,6 +19,7 @@ const happyTestCases: TestCase[] = [
       'ITEM000005',
       'ITEM000005-2',
     ],
+    expected: `***<store earning no money>Receipt ***
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
 Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
 Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
@@ -26,6 +27,23 @@ Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00
 Total：58.50(yuan)
 Discounted prices：7.50(yuan)
 **********************`
+  },
+  {
+    name: 'should print receipt without promotion when print receipt',
+    tags: [
+      'ITEM000003-100',
+      'ITEM000004-200',
+    ],
+    expected: `***<store earning no money>Receipt ***
+Name：Litchi，Quantity：100 pounds，Unit：15.00(yuan)，Subtotal：1500.00(yuan)
+Name：Battery，Quantity：200 as，Unit：2.00(yuan)，Subtotal：400.00(yuan)
+----------------------
+Total：1900.00(yuan)
+Discounted prices：0.00(yuan)
+**********************`
+  },
+]
+
 ]
 
 describe('printReceipt', () => {
