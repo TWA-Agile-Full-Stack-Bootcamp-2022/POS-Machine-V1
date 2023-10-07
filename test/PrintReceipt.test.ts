676a494b5bdda7dad4e3e8890ddf13bc270d448e
Hong Wu <hong.wu@rea-group.com>
2023-10-07 14:51:39 +0800

    test: create happy path test case

--- test/PrintReceipt.test.ts
+++ test/PrintReceipt.test.ts
@@ -1,8 +1,15 @@
 import {printReceipt} from '../src/PrintReceipt'
 
-describe('printReceipt', () => {
-  it('should print receipt with promotion when print receipt', () => {
-    const tags = [
+type TestCase = {
+  name: string
+  tags: string[]
+  expected?: string
+}
+
+const happyTestCases: TestCase[] = [
+  {
+    name: 'should print receipt with promotion when print receipt',
+    tags: [
       'ITEM000001',
       'ITEM000001',
       'ITEM000001',
@@ -11,9 +18,7 @@ describe('printReceipt', () => {
       'ITEM000003-2.5',
       'ITEM000005',
       'ITEM000005-2',
-    ]
-
-    const expectText = `***<store earning no money>Receipt ***
+    ],
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
 Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
 Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
@@ -21,7 +26,20 @@ Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00
 Total：58.50(yuan)
 Discounted prices：7.50(yuan)
 **********************`
+]
 
-    expect(printReceipt(tags)).toEqual(expectText)
+describe('printReceipt', () => {
+  happyTestCases.forEach(testCase => {
+    it(testCase.name, () => {
+      // given
+      const tags = testCase.tags
+
+      // when
+      const receipt = printReceipt(tags)
+
+      // then
+      expect(receipt).toEqual(testCase.expected)
+    })
+  })
   })
 })
