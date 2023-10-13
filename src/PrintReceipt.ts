620cbbe62357c2a897c5ef41c082c68f909bc246
Yixi <lyh1112@gmail.com>
2023-10-13 16:12:50 +0800

    feat: combine all functions to finish print receipt function

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -21,14 +21,12 @@ type Tag = {
 }
 
 export function printReceipt(tags: string[]): string {
-  return `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`
+  const parsedTags = calculateQuantity(tags)
+  const checkedTags = checkTags(parsedTags, loadAllItems())
+  const totalPriceTags = calculateTotalPrice(checkedTags)
+  const discountTags = calculateDiscount(totalPriceTags, loadPromotions())
+  const receipt = generateReceipt(discountTags)
+  return receipt
 }
 
 export function calculateQuantity(tags: string[]): Tag[] {
