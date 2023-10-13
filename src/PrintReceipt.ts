0d59271934add768cb564bb8782b913fd8828ae5
Yixi <lyh1112@gmail.com>
2023-10-13 10:58:14 +0800

    feat: add a calculate quantity function

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,5 +1,13 @@
 import {loadAllItems, loadPromotions} from './Dependencies'
 
+type Tag = {
+  barcode: string
+  quantity: number
+  itemInfo?: ReturnType<typeof loadAllItems>[0]
+  totalPrice?: number
+  discount?: number
+}
+
 export function printReceipt(tags: string[]): string {
   return `***<store earning no money>Receipt ***
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
@@ -9,4 +17,21 @@ Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00
 Total：58.50(yuan)
 Discounted prices：7.50(yuan)
 **********************`
+}
+
+export function calculateQuantity(tags: string[]): Tag[] {
+  const quantityMap = tags.reduce((result, tag) => {
+    const [barcode, quantity] = tag.split('-')
+    if (result.get(barcode) !== undefined) {
+      result.set(barcode, result.get(barcode)! + (Number(quantity) || 1))
+    } else {
+      result.set(barcode, Number(quantity) || 1)
+    }
+
+    return result
+  }, new Map<string, number>())
+
+  return Array.from(quantityMap.entries()).map(([barcode, quantity]) => {
+    return {barcode, quantity}
+  })
 }
